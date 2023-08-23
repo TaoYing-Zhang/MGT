2ddbf383f8167dcf04151927f6f7d60052dad6ff
TaoYing-Zhang <1581160248@qq.com>
2023-08-23 11:41:46 +0800

    second commit

--- data.py
+++ data.py
@@ -93,24 +93,10 @@ class TrainDataset(data.Dataset):
 
 
 def clamp(value, min=0., max=1.0):
-    """
-    将像素值强制约束在[0,1], 以免出现异常斑点
-    :param value:
-    :param min:
-    :param max:
-    :return:
-    """
     return torch.clamp(value, min=min, max=max)
 
 
 def RGB2YCrCb(rgb_image):
-    """
-    将RGB格式转换为YCrCb格式
-
-    :param rgb_image: RGB格式的图像数据
-    :return: Y, Cr, Cb
-    """
-
     R = rgb_image[0:1]
     G = rgb_image[1:2]
     B = rgb_image[2:3]
@@ -125,14 +111,6 @@ def RGB2YCrCb(rgb_image):
 
 
 def YCrCb2RGB(Y, Cb, Cr):
-    """
-    将YcrCb格式转换为RGB格式
-
-    :param Y:
-    :param Cb:
-    :param Cr:
-    :return:
-    """
     ycrcb = torch.cat([Y, Cr, Cb], dim=0)
     C, W, H = ycrcb.shape
     # print(ycrcb.shape)
